76da290d724250b5f6677af5d9a3c7e32e80ce37
Ariel <ariel.nevzglyad.hueso@gmail.com>
2024-03-08 17:14:10 +0100

    deploy fix

--- .vercel/output/functions/_render.func/chunks/generic_-I9ZxseF.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_QGai_Kar.mjs').then(n => n.g);
-
-export { page };
